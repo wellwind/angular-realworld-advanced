0a596d9f5f6549f1b2774efad2b80dce6586d822
wellwind <wellwind@mail2000.com.tw>
2018-05-20 21:07:47 +0800

    設定首頁與根目錄的路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import { AuthComponent } from './login/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+  },
+  {
+    path: 'login',
+    component: AuthComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
