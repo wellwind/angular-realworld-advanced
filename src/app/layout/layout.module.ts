95075cc2cfbc3c764fcac742a860e8717a89e13a
wellwind <wellwind@mail2000.com.tw>
2018-05-20 20:42:36 +0800

    LayoutModule 中匯入 RouterModule

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
-import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [NavComponent, FooterComponent, MainComponent],
   exports: [NavComponent, FooterComponent]
 })
