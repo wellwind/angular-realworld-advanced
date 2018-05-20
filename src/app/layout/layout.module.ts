14f1882ff9f9751ea612a1d4ac0aacc1ef5b7dbf
wellwind <wellwind@mail2000.com.tw>
2018-05-20 17:41:49 +0800

    建立 NavComponent 與 FooterComponent

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NavComponent, FooterComponent]
 })
 export class LayoutModule { }
