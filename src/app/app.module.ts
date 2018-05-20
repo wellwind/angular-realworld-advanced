ca98b12be542ea8b3c164fae83b89b3886f70296
wellwind <wellwind@mail2000.com.tw>
2018-05-20 20:56:33 +0800

    AppModule 中加入 LoginModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent]
 })
