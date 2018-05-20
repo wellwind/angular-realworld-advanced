7118223a2ed23e110e9bb00a62e4386a52b91305
wellwind <wellwind@mail2000.com.tw>
2018-05-20 20:37:37 +0800

    在 LayoutModule 下建立 MainComponent

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
 import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavComponent, FooterComponent],
+  declarations: [NavComponent, FooterComponent, MainComponent],
   exports: [NavComponent, FooterComponent]
 })
 export class LayoutModule {}
