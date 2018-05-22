044bac2794c0c416c56a3dd9bfd444a404b8a668
wellwind <wellwind@mail2000.com.tw>
2018-05-22 10:27:01 +0800

    移動 post 相關路由

--- src/app/posts/posts-routing.module.ts
+++ src/app/posts/posts-routing.module.ts
@@ -1,10 +1,32 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Route } from '@angular/router';
+import { EditorComponent } from './editor/editor.component';
+import { PostComponent } from './post/post.component';
+import { PostsComponent } from './posts/posts.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full' // prefix, full
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'post/:id',
+    component: PostComponent
+  },
+  {
+    path: 'create',
+    component: EditorComponent
+  }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: []
 })
-export class PostsRoutingModule { }
+export class PostsRoutingModule {}
