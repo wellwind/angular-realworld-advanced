0c0fbf9ff40cfa2ecf590596eecba2d15660122b
wellwind <wellwind@mail2000.com.tw>
2018-05-22 10:38:03 +0800

    將 PostsModule 從 AppModule 移除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
 import { LoginModule } from './login/login.module';
-import { PostsModule } from './posts/posts.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule, LoginModule, PostsModule],
+  imports: [BrowserModule, AppRoutingModule, LayoutModule, LoginModule],
   providers: [],
   bootstrap: [AppComponent]
 })
