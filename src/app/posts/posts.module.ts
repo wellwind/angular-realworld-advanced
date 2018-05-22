d545e17087d2f5980720e131032261613157bb72
wellwind <wellwind@mail2000.com.tw>
2018-05-22 11:42:53 +0800

    建立 PostsRoutingModule
    ng g m posts/posts-routing --flat --module=posts

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 import { PostComponent } from './post/post.component';
+import { PostsRoutingModule } from './posts-routing.module';
 import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, PostsRoutingModule],
   declarations: [PostsComponent, PostComponent, EditorComponent],
   exports: [PostsComponent, PostComponent, EditorComponent]
 })
