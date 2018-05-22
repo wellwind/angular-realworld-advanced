c52aabeb5d59225065999df17f92b1f347ab0a39
wellwind <wellwind@mail2000.com.tw>
2018-05-22 11:27:35 +0800

    PostsModule 中加入 RouterModule

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { EditorComponent } from './editor/editor.component';
 import { PostComponent } from './post/post.component';
 import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [PostsComponent, PostComponent, EditorComponent],
   exports: [PostsComponent, PostComponent, EditorComponent]
 })
